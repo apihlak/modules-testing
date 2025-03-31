892a48e53778cfb05910b646ec344ce2bc3b04dc
apihlak <apihlak@gmail.com>
2025-03-31 17:34:39 +0300

    test

--- modules/aws/vpc/generic/main.tf
+++ modules/aws/vpc/generic/main.tf
@@ -1,6 +1,6 @@
 # Hello
 module "label" {
-  source = "git::ssh://git@github.com/coingaming/infra-modules.git?ref=modules/global/label/v1.0.0"
+  source = "git::ssh://git@github.com/apihlak/modules-testing.git?ref=modules/global/label/v1.0.0"
   context = module.this.context
 }
 
